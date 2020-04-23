72805d6cc414a13ff4aeef2959e2757784f1360b
s0cr4t3sd <smr.jamc@gmail.com>
2020-04-23 18:47:52 +0200

    Añado atributos en la clase Persona

---
+++ src/main/java/com/mycompany/tarea4_joseantoniomoralescoll/Persona.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.tarea4_joseantoniomoralescoll;
+
+/**
+ *
+ * @author moral
+ */
+public class Persona {
+    private String nombre, apellido;
+    private int edad;
+}
