fd64fdb21e8a8787371d7077cfb93da8e88d5cf0
nao <zarza.naomi@gmail.com>
2020-04-23 19:32:10 +0200

    Añade ampliación al método Main

--- src/main/java/com/mycompany/tarea4_joseantoniomoralescoll/Prueba.java
+++ src/main/java/com/mycompany/tarea4_joseantoniomoralescoll/Prueba.java
@@ -12,7 +12,14 @@ package com.mycompany.tarea4_joseantoniomoralescoll;
 public class Prueba {
 
     public static void main(String[] args) {
+        //Creo e instancio tres personas
         Persona persona1 = new Persona("María Jesús", "García", 39);
-        System.out.println(persona1.toString());
+        Persona persona2 = new Persona("Javier", "Muñoz", 25);
+        Persona persona3 = new Persona();
+        
+        //Imprimo los datos
+        System.out.println("Datos persona1: " + persona1.toString());
+        System.out.println("Datos persona2: " + persona2.toString());
+        System.out.println("Datos persona3: " + persona3.toString());
     }
 }
