02f4aac94c48fc328e2f988b2e92b901a414107f
nao <zarza.naomi@gmail.com>
2020-04-23 19:24:23 +0200

    Añade Clase Prueba con el Main al proyecto del compañero

---
+++ src/main/java/com/mycompany/tarea4_joseantoniomoralescoll/Prueba.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.tarea4_joseantoniomoralescoll;
+
+/**
+ *
+ * @author Nao
+ */
+public class Prueba {
+
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("María Jesús", "García", 39);
+        System.out.println(persona1.toString());
+    }
+}
