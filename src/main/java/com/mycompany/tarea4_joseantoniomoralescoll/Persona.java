607642bf628b7d3fb73968f87591e63b7572455a
s0cr4t3sd <smr.jamc@gmail.com>
2020-04-23 19:02:05 +0200

    Añade constructores en clase Persona

--- src/main/java/com/mycompany/tarea4_joseantoniomoralescoll/Persona.java
+++ src/main/java/com/mycompany/tarea4_joseantoniomoralescoll/Persona.java
@@ -12,4 +12,18 @@ package com.mycompany.tarea4_joseantoniomoralescoll;
 public class Persona {
     private String nombre, apellido;
     private int edad;
+    
+    public Persona(){
+        this.nombre = "Sin nombre";
+        this.apellido = "Sin apellido";
+        this.edad = 0;
+    }
+
+    public Persona(String nombre, String apellido, int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+    
+    
 }
